50bec98612532b4efc8731958c00e606253ec19a
BurntIce16 <clayton.midgley@gmail.com>
2023-07-16 12:37:08 -0400

    Added ability to index and load games from node_modules

--- Host/bundle.js
+++ Host/bundle.js
@@ -1,3 +1,85 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-console.log("Server Started");
+//host screen manager
+//This will handle the game selection screen, and the interfaces for the games themselves
+
+let serverws = null;
+let clientID;
+
+connectWS();
+
+function connectWS() {
+	//lets ws work over localhost and ipv4 (an in theory dedicated dns but that will require more work)
+	let url = new URL(window.location.href);
+	url.port = "8080";
+	url.protocol = "ws";
+	serverws = new WebSocket(url, 13);
+
+	//check if cookies have any data
+	let cookie = document.cookie;
+	cookie = cookie.split("id=")[1];
+	cookie = cookie.split("; undefined")[0];
+	console.log(cookie);
+	clientID = cookie;
+	
+
+	serverws.onopen = () => {
+		console.log("Connected to server");
+        //TODO elevate this ws from a client to a host
+	};
+
+	serverws.onmessage = (message) => {
+		//console.log(`Received message: ${message.data}`);
+		handleData(message.data);
+	};
+
+	serverws.onclose = () => {
+		handleData(JSON.parse(`{'type':'interface', 'data':'<h1>Server Has Closed</h1>'}`));
+	}
+}
+
+function handleData(data) {
+	data = JSON.parse(data);
+	//handle data from server
+	if (data.type == "interface") {
+		//replace all html within the interface div with the html under data.data
+		interfaceDiv.innerHTML = data.data;
+	}
+}
+
+function messageFormater(type, data){
+	return JSON.stringify({
+		type:type,
+		data:data
+	});
+}
+
+
+//Click input manager because buttons are lame
+document.addEventListener( "click", clickListener );
+
+function clickListener(event){
+    var element = event.target;
+	//get element id
+	let id = element.id;
+
+
+    //TODO UPDATE THIS SECTION
+	switch(id){
+		case "joinGame":
+			console.log(clientID);
+			serverws.send(messageFormater("joinGame", clientID));
+			break;
+		case "changeName":
+			//check if we are requesting the change name page or if we are submitting a name change
+			if(!document.getElementById("nameInput")){
+				serverws.send(messageFormater("navigate", "changeName"));
+			}else{
+				let name = document.getElementById("nameInput").value;
+				document.getElementById("topText").innerHTML = name;
+				serverws.send(messageFormater("changeName", name));
+			}
+		default:
+			break;
+	}	
+}
 },{}]},{},[1]);
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-import express from 'express';
-import WebSocket, { WebSocketServer } from 'ws';
-import cookieParser from 'cookie-parser';
-import { nanoid } from 'nanoid';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import WebSocket, { WebSocketServer } from "ws";
+import cookieParser from "cookie-parser";
+import { nanoid } from "nanoid";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import * as fs from 'fs';
+import {fork} from 'child_process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+
+let interfaces = {};
+loadInterfaces();
+let games = {};
+loadGames();
 
 //create a new express app
 const app = express();
@@ -19,14 +27,14 @@ app.get("/", (req, res) => {
 	let id = req.cookies.id;
 
 	//give client an id if they dont have one
-	if(req.cookies.id == undefined){
+	if (req.cookies.id == undefined) {
 		id = nanoid();
 		res.cookie(`id`, id);
 	}
 	//check if id is not in use, if so add it to the ids object
-	if(ids[id] === undefined){
+	if (ids[id] === undefined) {
 		addID(id);
-	};
+	}
 	//console.log(ids);
 	res.sendFile("Client/index.html", { root: __dirname });
 });
@@ -46,12 +54,10 @@ app.get("/IBMPlexSans-SemiBold.ttf", (req, res) => {
 	res.sendFile("Client/IBMPlexSans-SemiBold.ttf", { root: __dirname });
 });
 
-
 app.listen(port, () => {
 	//server starts listening for any attempts from a client to connect at port: {port}
 	console.log(`Server now listening on port ${port}`);
 });
-
 
 const hostApp = express();
 const hostPort = 3001;
@@ -80,19 +86,13 @@ hostApp.listen(hostPort, () => {
 	console.log(`Host is now listening on port ${hostPort}`);
 });
 
-
 //create a new websocket server
 const wss = new WebSocketServer({ port: 8080 });
-
-
 
 //list of id that the server has recognized in id:name format
 //{id:{name:"some name", player:true}}
 //player boolean, true = player, false = spectator
 let ids = {};
-
-
-
 
 //when a client connects to the server, print a message in the console
 wss.on("connection", (ws) => {
@@ -102,7 +102,6 @@ wss.on("connection", (ws) => {
 	//set client to pre lobby screen
 	updateClientInterface(ws, interfaces.preLobby);
 
-
 	//Handlers and other boring shit
 	ws.on("close", () => {
 		//console.log("Client disconnected");
@@ -111,7 +110,7 @@ wss.on("connection", (ws) => {
 
 	ws.on("message", (data) => {
 		data = JSON.parse(data);
-		switch(data.type){
+		switch (data.type) {
 			case "joinGame":
 				console.log(data.data);
 				//associate ws client with clientID
@@ -127,46 +126,74 @@ wss.on("connection", (ws) => {
 			case "navigate":
 				updateClientInterface(ws, interfaces[data.data]);
 				break;
+			case "launchGame":
+				console.log("Recieved Game Name: ", data.data);
+				launchGame(data.data);
+				break;
 			default:
 				console.log("unknown type");
 				console.log(data);
 				break;
 		}
-
 	});
 });
 
-function messageFormater(type, data){
+function messageFormater(type, data) {
 	return JSON.stringify({
-		type:type,
-		data:data
+		type: type,
+		data: data,
 	});
 }
 
-function updateClientInterface(client, interfaceState){
+function updateClientInterface(client, interfaceState) {
 	client.send(messageFormater("interface", interfaceState));
 }
 
-function updateAllClientInterfaces(interfaceState){
+function updateAllClientInterfaces(interfaceState) {
 	wss.clients.forEach((client) => {
 		client.send(messageFormater("interface", interfaceState));
 	});
 }
 
-
-
-function addID(id, name = "player", player = false){
+function addID(id, name = "player", player = false) {
 	ids[id] = {
 		name: name,
-		player: player
-	}
+		player: player,
+	};
 }
 
-let interfaces = {
-	
-	preLobby:`<input class="textInput" id="roomCode" type="text" onfocus="this.value=''" value="CODE" maxlength="4"><div id="joinGame" class="button">JOIN GAME</div>`,
-	lobby:`<div class="text">WAITING FOR HOST</div><div id="changeName" class="button">CHANGE NAME</div>`,
-	changeName:`<input class="textInput" id="nameInput" type="text" onfocus="this.value=''" value="PLAYER" maxlength="10"></input><div id="changeName" class="button">CHANGE NAME</div>`
 
+
+function loadInterfaces() {
+	//get all html files in Client/interfaces and convert them to strings
+	fs.readdir("Client/Interfaces", (err, files) => {
+		files.forEach((file) => {
+			let fileName = file.split(".")[0];
+			interfaces[fileName] = fs.readFileSync(`Client/Interfaces/${file}`, "utf8");
+		});
+	});
 }
 
+function loadGames() {
+	//Scan through all top level node modules and check the package.json for "game": true
+	fs.readdir("node_modules", (err, files) => {
+		files.forEach((file) => {
+			//open package.json file in folder as an object
+			try{
+				let packageJson = JSON.parse(fs.readFileSync(`node_modules/${file}/package.json`, "utf8"));
+				//check if package.json has game: true
+				if (packageJson.game) {
+					//TODO add game to games list
+					games[packageJson.name] = packageJson.author;
+				}
+			}catch(err){
+				//do nothing
+			}
+		});
+	});
+}
+
+function launchGame(game){
+	console.log(game);
+	var child = fork(`node_modules/${game}/index.js`);
+}
